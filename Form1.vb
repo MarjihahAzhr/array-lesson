eaab3bda44f4cc70503304b0224f37279d50580c
Marjihah Binti Azahar <18DDT19F1081@student.ptss.edu.my>
2021-04-19 14:23:53 +0800

    array susunan hari

---
+++ Form1.vb
@@ -0,0 +1,19 @@
+﻿Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim strnamahari(6) As String
+        Dim a As Integer
+
+        strnamahari(0) = "ahad"
+        strnamahari(1) = "isnin"
+        strnamahari(2) = "selasa"
+        strnamahari(3) = "rabu"
+        strnamahari(4) = "khamis"
+        strnamahari(5) = "jumaat"
+        strnamahari(6) = "sabtu"
+        Array.Reverse(strnamahari)
+
+        For a = 0 To 6
+            ListBox1.Items.Add(strnamahari(a))
+        Next
+    End Sub
+End Class
